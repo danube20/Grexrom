a82c7257983890535d4119f2636f45ba6f9d06a1
monica <monicasastudy@gmail.com>
2022-02-14 17:34:40 +0100

    added auth

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -8,6 +8,7 @@ const rounds = 10
 router.get("/register", (req, res, next) => res.render("auth/resgister-form"))
 
 router.post("/register", (req, res, next) => {
+
     const { email, username, userPassword } = req.body
 
     bcryptjs
@@ -17,5 +18,33 @@ router.post("/register", (req, res, next) => {
             return User.create({ email, username, password: hashPassword })
         })
         .then(() => res.redirect("/login"))
+        .catch(error => next(error))
+})
+
+router.get("/login", (req, res, next) => res.render("auth/login-form"))
+
+router.post("/login", (req, res, next) => {
+
+    const { username, userPassword } = req.body
+
+    if (email.length === 0 || userPassword.length === 0) {
+        res.render("auth/resgister-form", { errorMessage: "Rellena todos los campos" })
+        return
+    }
+
+    User
+        .findOne({ email })
+        .then(user => {
+            if (!user) {
+                res.render("/auth/login-form", { errorMessage: "Email no registrado" })
+                return
+            } else if (bcryptjs.compareSync(userPassword, hashPassword) === false) {
+                res.render("/auth/login-form", { errorMessage: "Contraseña incorrecta" })
+                return
+            } else {
+                req.session.currentUser = user
+                res.redirect("/")
+            }
+        })
         .catch(error => next(error))
 })
