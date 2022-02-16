5bd95f380de7b64ca4d6c71065abbdee8e5c2fa0
Andrés García <playsharp59@gmail.com>
2022-02-16 10:50:46 +0100

    added default image profile

--- models/User.model.js
+++ models/User.model.js
@@ -31,7 +31,10 @@ const userSchema = new Schema(
             type: String,
             maxlength: 200
         },
-        imgUrl: String
+        imgUrl: {
+            type: String,
+            default: 'https://t4.ftcdn.net/jpg/00/64/67/63/360_F_64676383_LdbmhiNM6Ypzb3FM4PPuFP9rHe7ri8Ju.jpg'
+        }
     },
     {
         timestamps: true
